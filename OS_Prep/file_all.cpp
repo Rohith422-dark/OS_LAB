0b63dc535b8c2dfeb73c26a065482f39a8b76e0c
Rohith422-dark <devireddyrohith@gmail.com>
2026-04-21 18:33:07 +0530

    Update file_all.cpp

--- OS_Prep/file_all.cpp
+++ OS_Prep/file_all.cpp
@@ -1,117 +1,105 @@
-
-
-#include<iostream>
-#include<vector>
+#include <bits/stdc++.h>
 using namespace std;
-
-int main(){
-  int diskSize; 
-  cout<<"Disk size: "; 
-  cin>>diskSize;
-
-  vector<bool> used(diskSize,false);
-
-  int occ; 
-  cout<<"Pre-occupied blocks: "; 
-  cin>>occ;
-
-  for(int i=0;i<occ;i++){ 
-    int b; 
-    cin>>b; 
-    used[b]=true; 
-  }
-
-  int nf; 
-  cout<<"Files: "; 
-  cin>>nf;
-
-  // ✅ fixed (no VLA)
-  vector<string> fname(nf);  
-  vector<int> need(nf);
-
-  for(int i=0;i<nf;i++){
-    cout<<"Name blocks for file "<<i+1<<": ";
-    cin>>fname[i]>>need[i];
-  }
-
-  auto getFree=[&](vector<int>&v,int cnt,vector<bool>&u){
-    for(int i=0;i<diskSize&&(int)v.size()<cnt;i++)
-      if(!u[i]){ v.push_back(i); u[i]=true; }
-  };
-
-  // Sequential
-  cout<<"\n--- Sequential ---\n";
-  vector<bool> uS=used;
-
-  for(int i=0;i<nf;i++){
-    bool ok=false;
-
-    for(int s=0;s<=diskSize-need[i];s++){
-      bool fr=true;
-
-      for(int k=s;k<s+need[i];k++)
-        if(uS[k]){ fr=false; break; }
-
-      if(fr){
-        cout<<fname[i]<<": ";
-        for(int k=s;k<s+need[i];k++){
-          cout<<k<<" ";
-          uS[k]=true;
+int main() {
+    int n;
+    cout << "Enter Number of Locations: ";
+    cin >> n;
+    vector<bool> disk(n);
+    cout << "Enter disk (0/1): ";
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        disk[i] = (x == 1);
+    }
+    cout << "1. seq\n2. link\n3. inde\n";
+    int c;
+    cout << "enter option: ";
+    cin >> c;
+    int num;
+    cout << "enter number of files: ";
+    cin >> num;
+    vector<int> files(num);
+    cout << "Enter file sizes: ";
+    for (int i = 0; i < num; i++) cin >> files[i];
+    for (int f = 0; f < num; f++) {
+        int s = files[f];
+        cout << "f size: " << s << endl;
+        if (c == 1) {  // Sequential
+            bool acc = false;
+            for (int i = 0; i <= n - s; i++) {
+                bool free = true;
+                for (int j = i; j < i + s; j++) {
+                    if (disk[j]) {
+                        free = false;
+                        break;
+                    }
+                }
+                if (free) {
+                    for (int j = i; j < i + s; j++)
+                        disk[j] = true;
+                    cout << "Allocated blocks: ";
+                    for (int j = i; j < i + s; j++)
+                        cout << j << " ";
+                    cout << endl;
+                    cout << "Stored at positions: "
+                         << i << " to " << i + s - 1 << endl;
+                    acc = true;
+                    break;
+                }
+            }
+            if (!acc) cout << "no space left\n";
         }
-        cout<<"\n";
-        ok=true;
-        break;
-      }
+        else if (c == 2) {  // Linked
+            vector<int> free;
+            for (int i = 0; i < n; i++)
+                if (!disk[i]) free.push_back(i);
+            if (free.size() < s) {
+                cout << "no space left\n";
+            } else {
+                vector<int> accblock(free.begin(), free.begin() + s);
+                for (int x : accblock) disk[x] = true;
+                cout << "Allocated blocks (linked):\n";
+                for (int i = 0; i < accblock.size(); i++) {
+                    cout << accblock[i];
+                    if (i != accblock.size() - 1) cout << " -> ";
+                }
+                cout << endl;
+                cout << "Stored at positions: ";
+                for (int x : accblock) cout << x << " ";
+                cout << endl;
+            }
+        }
+        else if (c == 3) {  // Indexed
+            vector<int> free;
+            for (int i = 0; i < n; i++)
+                if (!disk[i]) free.push_back(i);
+            if (free.size() < s + 1) {
+                cout << "no space left\n";
+            } else {
+                int index = free[0];
+                disk[index] = true;
+                vector<int> accblock(free.begin() + 1, free.begin() + 1 + s);
+                for (int x : accblock) disk[x] = true;
+                cout << "indexblock: " << index << endl;
+                cout << "datablocks: [";
+                for (int i = 0; i < accblock.size(); i++) {
+                    cout << accblock[i];
+                    if (i != accblock.size() - 1) cout << ", ";
+                }
+                cout << "]\n";
+            }
+        }
+        else {
+            cout << "invalid\n";
+            break;
+        }
+        // Print disk state
+        cout << "[";
+        for (int i = 0; i < n; i++) {
+            cout << "'" << (disk[i] ? "1" : "0") << "'";
+            if (i != n - 1) cout << ", ";
+        }
+        cout << "]\n";
     }
-
-    if(!ok) cout<<fname[i]<<": Failed\n";
-  }
-
-  // Indexed
-  cout<<"\n--- Indexed ---\n";
-  vector<bool> uI=used;
-
-  for(int i=0;i<nf;i++){
-    vector<int> b;
-    getFree(b,need[i]+1,uI);
-
-    if((int)b.size()<need[i]+1){
-      cout<<fname[i]<<": Failed\n";
-      continue;
-    }
-
-    cout<<fname[i]<<" inode="<<b[0]<<" data: ";
-    for(int k=1;k<=need[i];k++) cout<<b[k]<<" ";
-    cout<<"\n";
-  }
-
-  // Linked
-  cout<<"\n--- Linked ---\n";
-  vector<bool> uL=used;
-
-  for(int i=0;i<nf;i++){
-    vector<int> b;
-    getFree(b,need[i],uL);
-
-    if(b.empty()){
-      cout<<fname[i]<<": Failed\n";
-      continue;
-    }
-
-    cout<<fname[i]<<": ";
-    for(int k=0;k<(int)b.size()-1;k++)
-      cout<<b[k]<<"->";
-
-    cout<<b.back()<<"->NULL\n";
-  }
-
-  /*
-  Gantt chart not applicable for file allocation.
-  Output itself is block diagram:
-  Sequential → blocks in order
-  Indexed → inode -> blocks
-  Linked → chain of blocks
-  */
-
-  return 0;
+    return 0;
 }
