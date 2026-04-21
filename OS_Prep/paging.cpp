626ca907b3ef2e352749195e9196566457e31f9e
Rohith422-dark <devireddyrohith@gmail.com>
2026-04-21 22:30:31 +0530

    Update paging.cpp

--- OS_Prep/paging.cpp
+++ OS_Prep/paging.cpp
@@ -1,21 +1,46 @@
 #include<iostream>
 #include<cmath>
+#include<bitset>
 using namespace std;
+
+int bits(int x){
+    int b = 0;
+    while((1 << b) < x) b++;
+    return b;
+}
 int main(){
-  int ps,pg,nf;
-  cout<<"Process size: "; cin>>ps;
-  cout<<"Page/Frame size: "; cin>>pg;
-  cout<<"Frames in memory: "; cin>>nf;
-  int np=(int)ceil((double)ps/pg);
-  cout<<"\nPages required: "<<np<<"\n";
-  if(np>nf){ cout<<"Not enough frames!\n"; return 0; }
-  int pt[np];
-  cout<<"Enter page table (frame for each page):\n";
-  for(int i=0;i<np;i++){ cout<<"Page "<<i<<": "; cin>>pt[i]; }
-  cout<<"\nLogical -> Physical\n";
-  for(int la=0;la<ps;la++){
-    int page=la/pg, offset=la%pg;
-    int pa=pt[page]*pg+offset;
-    cout<<"LA="<<la<<" (P"<<page<<",Off="<<offset<<") -> PA="<<pa<<"\n";
-  }
+    int ps, pg, nf;
+    cout<<"Process size: ";
+    cin>>ps;
+    cout<<"Page/Frame size: ";
+    cin>>pg;
+    cout<<"Frames in memory: ";
+    cin>>nf;
+    int np = ceil((double)ps / pg);
+    cout<<"\nPages required: "<<np<<"\n";
+    if(np > nf){
+        cout<<"Not enough frames!\n";
+        return 0;
+    }
+    int pt[np];
+    cout<<"Enter page table:\n";
+    for(int i=0;i<np;i++){
+        cout<<"Page "<<i<<": ";
+        cin>>pt[i];
+    }
+    int offset_bits = bits(pg);
+    int page_bits   = bits(np);
+    int frame_bits  = bits(nf);
+    cout<<"\nLogical -> Physical (Binary)\n\n";
+    for(int la=0; la<ps; la++){
+        int page = la / pg;
+        int offset = la % pg;
+        int frame = pt[page];
+        int pa = frame * pg + offset;
+        cout<<"Byte "<<la<<" ("<<bitset<8>(la)<<") -> ";
+        cout<<"(Page "<<page<<" ("<<bitset<4>(page)<<"), ";
+        cout<<"Offset "<<offset<<" ("<<bitset<4>(offset)<<")) -> ";
+        cout<<"Frame "<<frame<<" ("<<bitset<4>(frame)<<") -> ";
+        cout<<"Physical Address "<<pa<<" ("<<bitset<8>(pa)<<")\n";
+    }
 }
