9137fcfa88842cc0b5a3810d74b2d311041e83fe
Rohith422-dark <devireddyrohith@gmail.com>
2026-05-14 23:52:22 +0530

    Update page_replace.cpp

--- OS_Prep/page_replace.cpp
+++ OS_Prep/page_replace.cpp
@@ -94,27 +94,44 @@ void optimal(){
       h++;
       printRow(mem,pages[i],'H');
     }
-    else{
-      f++;
-      int rep=0,far=-1;
+   else{
+    f++;
 
-      for(int j=0;j<frames;j++){
-        int nx=INT_MAX;
-        for(int k=i+1;k<n;k++){
-          if(pages[k]==mem[j]){
-            nx=k;
+    int rep=-1;
+
+    
+    for(int j=0;j<frames;j++){
+        if(mem[j]==-1){
+            rep=j;
             break;
-          }
         }
-        if(nx>far){
-          far=nx;
-          rep=j;
-        }
-      }
-
-      mem[rep]=pages[i];
-      printRow(mem,pages[i],'F');
     }
+
+    
+    if(rep==-1){
+        int far=-1;
+
+        for(int j=0;j<frames;j++){
+            int nx=INT_MAX;
+
+            for(int k=i+1;k<n;k++){
+                if(mem[j]==pages[k]){
+                    nx=k;
+                    break;
+                }
+            }
+
+            if(nx>far){
+                far=nx;
+                rep=j;
+            }
+        }
+    }
+
+    mem[rep]=pages[i];
+    printRow(mem,pages[i],'F');
+}
+    
   }
   cout<<"Faults="<<f<<" Hits="<<h<<"\n";
 }
